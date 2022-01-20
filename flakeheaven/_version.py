e2241fa7dbf6542bca8f6f93d562bdfa897e5a4d
Jose Rojas <39174181+jarojasm95@users.noreply.github.com>
2022-01-19 18:41:08 -0700

    mypy fix

--- flakeheaven/_version.py
+++ flakeheaven/_version.py
@@ -4,6 +4,6 @@ try:
 except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     # external
-    import importlib_metadata as metadata
+    import importlib_metadata as metadata  # type: ignore
 
 __version__ = metadata.version("flakeheaven")
